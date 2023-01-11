429103f1170f3a31a6fbce091e1564372ae6501f
Brandon Calderon <122395570+Brandonxcu87@users.noreply.github.com>
2023-01-10 21:54:52 -0600

    Add files via upload

---
+++ WeAppCartago/Login.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WeAppCartago
+{
+    partial class Frm_Ingreso
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Ingreso));
+            this.txb_usuario = new System.Windows.Forms.TextBox();
+            this.txb_contraseña = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_Asistencia = new System.Windows.Forms.Button();
+            this.btn_Ingresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txb_usuario
+            // 
+            this.txb_usuario.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txb_usuario.BackColor = System.Drawing.Color.Black;
+            this.txb_usuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txb_usuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(181)))), ((int)(((byte)(59)))));
+            this.txb_usuario.Location = new System.Drawing.Point(85, 102);
+            this.txb_usuario.MaximumSize = new System.Drawing.Size(100, 20);
+            this.txb_usuario.Name = "txb_usuario";
+            this.txb_usuario.Size = new System.Drawing.Size(100, 20);
+            this.txb_usuario.TabIndex = 0;
+            this.txb_usuario.Text = "Usuario";
+         
+            // 
+            // txb_contraseña
+            // 
+            this.txb_contraseña.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txb_contraseña.BackColor = System.Drawing.Color.Black;
+            this.txb_contraseña.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txb_contraseña.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(181)))), ((int)(((byte)(59)))));
+            this.txb_contraseña.Location = new System.Drawing.Point(213, 102);
+            this.txb_contraseña.MaximumSize = new System.Drawing.Size(100, 20);
+            this.txb_contraseña.Name = "txb_contraseña";
+            this.txb_contraseña.Size = new System.Drawing.Size(100, 20);
+            this.txb_contraseña.TabIndex = 1;
+            this.txb_contraseña.Text = "Contraseña";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(181)))), ((int)(((byte)(59)))));
+            this.label1.Location = new System.Drawing.Point(84, 54);
+            this.label1.MaximumSize = new System.Drawing.Size(100, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bienvenido ";
+            // 
+            // Btn_Asistencia
+            // 
+            this.Btn_Asistencia.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Btn_Asistencia.FlatAppearance.BorderSize = 0;
+            this.Btn_Asistencia.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(181)))), ((int)(((byte)(59)))));
+            this.Btn_Asistencia.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(161)))), ((int)(((byte)(49)))));
+            this.Btn_Asistencia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Asistencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Asistencia.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(181)))), ((int)(((byte)(59)))));
+            this.Btn_Asistencia.Location = new System.Drawing.Point(257, 191);
+            this.Btn_Asistencia.MaximumSize = new System.Drawing.Size(329, 23);
+            this.Btn_Asistencia.Name = "Btn_Asistencia";
+            this.Btn_Asistencia.Size = new System.Drawing.Size(229, 23);
+            this.Btn_Asistencia.TabIndex = 4;
+            this.Btn_Asistencia.Text = "Asistencia";
+            this.Btn_Asistencia.UseVisualStyleBackColor = true;
+            this.Btn_Asistencia.Click += new System.EventHandler(this.Btn_Asistencia_Click);
+            // 
+            // btn_Ingresar
+            // 
+            this.btn_Ingresar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Ingresar.FlatAppearance.BorderSize = 0;
+            this.btn_Ingresar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(181)))), ((int)(((byte)(59)))));
+            this.btn_Ingresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(161)))), ((int)(((byte)(49)))));
+            this.btn_Ingresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Ingresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ingresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(181)))), ((int)(((byte)(59)))));
+            this.btn_Ingresar.Location = new System.Drawing.Point(22, 191);
+            this.btn_Ingresar.MaximumSize = new System.Drawing.Size(329, 23);
+            this.btn_Ingresar.Name = "btn_Ingresar";
+            this.btn_Ingresar.Size = new System.Drawing.Size(229, 23);
+            this.btn_Ingresar.TabIndex = 5;
+            this.btn_Ingresar.Text = "Ingresar";
+            this.btn_Ingresar.UseVisualStyleBackColor = true;
+            this.btn_Ingresar.Click += new System.EventHandler(this.btn_Ingresar_Click);
+            // 
+            // Frm_Ingreso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(526, 226);
+            this.Controls.Add(this.btn_Ingresar);
+            this.Controls.Add(this.Btn_Asistencia);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txb_contraseña);
+            this.Controls.Add(this.txb_usuario);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(542, 265);
+            this.MinimumSize = new System.Drawing.Size(542, 265);
+            this.Name = "Frm_Ingreso";
+            this.Text = "WeApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txb_usuario;
+        private System.Windows.Forms.TextBox txb_contraseña;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_Asistencia;
+        private System.Windows.Forms.Button btn_Ingresar;
+    }
+}
+
---
+++ WeAppCartago/Login.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeAppCartago
+{
+    public partial class Frm_Ingreso : Form
+    {
+        public string usuario;
+      
+
+
+        private ClaseAdmins pro;
+        private ClaseAdminsHelper ProdH;
+        private DataTable table;
+        public Frm_Ingreso()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Asistencia_Click(object sender, EventArgs e)
+        {
+            Asistencia F_Asistencia = new Asistencia();
+            F_Asistencia.Show();
+            this.Hide();
+        }
+
+
+        private void ingreso()
+        {
+            pro = new ClaseAdmins();
+            usuario = this.txb_usuario.Text; 
+            pro.Usuario = this.txb_usuario.Text;
+            ProdH = new ClaseAdminsHelper(pro);
+            table = new DataTable();
+            table = ProdH.BuscarContraseña();
+            Principal F_clientes = new Principal();
+            F_clientes.usuario = usuario;
+            if (table.Rows.Count > 0)
+            {
+                DataRow row = table.Rows[0];
+                if (row["Contraseña"].ToString() == txb_contraseña.Text)
+                {              
+                    F_clientes.Show(this);
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("contraseña incorrecta");
+                    txb_contraseña.Text = "";
+                    txb_usuario.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("usuario no existe");
+            }
+        }
+
+
+        private void btn_Ingresar_Click(object sender, EventArgs e)
+        {
+            ingreso();
+        }
+    }
+}
